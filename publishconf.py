c923cf29626ceec55d890dfe93145a2306047691
Werberth Vinicius <werberthvinicius@gmail.com>
2017-08-13 23:50:39 -0300

    Changing the SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://github.com/Werberth/Werberth.github.io'
+SITEURL = 'https://werberth.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
